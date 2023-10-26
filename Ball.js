9c8faf5b1d3e90dbed95dbe3ad93dbc4f8de3263
sarahannlst@gmail.com <67523761+sarahannlst@users.noreply.github.com>
2023-10-26 12:09:43 -0700

    raymarching but not for multiple collisions in one time step (should be fine if obstacles are far from e/o

--- Ball.js
+++ Ball.js
@@ -18,24 +18,13 @@ class Ball {
 
 		circle(this.p.x, this.p.y, this.r);
 	}
-
+	
 	timestep(dt) {
 		// Symplectic Euler integrator:
 		// Update velocity:
 		const g = +9.81 * 0.113203 * height / 2; // where sin(6.5 degrees) = 0.113203
 		this.v.y += dt * g; // positive gravity makes it fall down (since coord system flipped vertically)
-
-		// From starter code: Process collisions :) 
-		let distO = distanceO(this.p);
-		let obstacle = distO[1];
-		let d = distO[0] - this.r;
-		let n = obstacle.normal(this.p);
-		let vn = this.v.dot(n) - obstacle.velocity(this.p).dot(n);
-
-		// Raymarching code
-		let time_remaining = dt; // this will be decremented
-		while (time_remaining > 0) {
-			// find the distance of the full time step
+	
 			let depthMax = this.v.mag() * dt  - this.r;
 			// normal velocity direction
 			let vHat = this.v.copy();
@@ -43,54 +32,55 @@ class Ball {
 			let p0 = this.p.copy();
 	
 			// distance of the raymarched step
-			let depth = this.raymarchTimestep(depthMax, vHat, p0, dt);
+			let depth = this.raymarchTimestep(depthMax, vHat, p0);
 			let time = (depth)/(this.v.mag()); // distance/speed = time
-			time_remaining -= time;
-		}
+			if (time < dt) {
+				acc(this.p, time, this.v);
+				let time_remaining = dt - time;
+				let distO = distanceO(this.p); 
+				let obstacle = distO[1]; // nearest object
+				let n = obstacle.normal(this.p);
+				let vn = this.v.dot(n) - obstacle.velocity(this.p).dot(n);
+				if (vn < 0) {
+					let eps = obstacle.getCOR();
+					vn *= -(1 + eps);
+					n.mult(vn);
+					this.v.add(n);
+					obstacle.notifyOfCollision();
+				}
+				if (time_remaining>0){
+					acc(this.p, time_remaining, this.v);
+				}
+			}
+			else {
+				acc(this.p, dt, this.v);
+			}
+			
 	}
-
-	raymarchTimestep(depthMax, vHat, p0, dt) {
-		// isPaused = true;
+	
+	raymarchTimestep(depthMax, vHat, p0) {
 		const MAX_MARCHING_STEPS = 30;
 		const PRECISION = 0.01;
-
+	
 		let depth = 0; // total distance moved on ray 
 		let distO = distanceO(p0); 
 		let obstacle = distO[1]; // nearest object
 		let d = 0; 
-
 		for (let i=0; i<MAX_MARCHING_STEPS; i++) {
-			// isPaused = true;
 			let vHatcpy = vHat.copy();
-			let p0 = this.p.copy();
 			vHatcpy.mult(depth); // march along v direction
 			p0.add(vHatcpy); // add delta v hat
 			distO = distanceO(p0);
 			obstacle = distO[1];
-			d = distO[0]; // distance perpendicular to obstacle
+			d = distO[0]- this.r; // distance perpendicular to obstacle
 			depth += d; // walk along ray “d”
-			if (d < PRECISION + this.r) { // small distance, exit and collide
-				break; 
+			if (d < PRECISION) { // small distance, exit and collide
+				break;
 			}
-
 			if (depth > depthMax) { 
-				acc(this.p, dt, this.v); // move the whole step
-				return depth;
+				break; // move the whole step
 			}
-			let time = (d)/(this.v.mag());
-			acc(this.p, time, this.v);	
 		}	
-		let n = obstacle.normal(this.p);
-		let vn = this.v.dot(n) - obstacle.velocity(this.p).dot(n);
-		if (vn < 0) {
-			let eps = obstacle.getCOR();
-			vn *= -(1 + eps);
-			n.mult(vn);
-			this.v.add(n);
-			obstacle.notifyOfCollision();
-		}
-		let time = (d)/(this.v.mag());
-		acc(this.p, time, this.v);
 		return depth;
 	}
 }
